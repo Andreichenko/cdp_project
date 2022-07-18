961ba965bb11b4fbd03b18d9c1e3fd1f4e3adab4
aleksandrandreichenko <andreichenko@web.de>
2022-07-18 21:51:53 +0300

    terraform IaC for jenkins

---
+++ terraform/outputs.tf
@@ -0,0 +1,16 @@
+output "aws_common_subnet_primary" {
+  value = aws_subnet.common_subnet_primary.id
+}
+
+output "internet-gateway-common" {
+  value = aws_internet_gateway.internet-gateway-common.id
+}
+
+output "vpc_common" {
+  value = aws_vpc.vpc_common.cidr_block
+}
+
+output "jenkins-master-node-public-ip" {
+  value = aws_instance.jenkins-master-node.public_ip
+}
+
