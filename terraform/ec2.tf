ed3cad79d8187e0b3465937d4c7ef57c4f6d24b6
aleksandrandreichenko <andreichenko@web.de>
2022-07-20 20:40:23 +0300

    CDP-23   + terraform ec2 instance and sg   + ansible playbook for tomcat-server

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,6 +3,11 @@ data "aws_ssm_parameter" "linuxAMI-us-east-1" {
   name                         = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
+data "aws_ssm_parameter" "Ubuntu-us-east-1"{
+  provider = aws.region-common
+  name = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20220616"
+}
+
 #create key-pair for logging into EC2 in us-east-1
 resource "aws_key_pair" "common-key" {
   provider                     = aws.region-common
@@ -10,7 +15,7 @@ resource "aws_key_pair" "common-key" {
   key_name                     = "jenkins"
 }
 
-#create and bootstrap ec2 in us-east-1
+#create and bootstrap ec2 in us-east-1 jenkins
 resource "aws_instance" "jenkins-master-node" {
   provider                     = aws.region-common
   ami                          = data.aws_ssm_parameter.linuxAMI-us-east-1.value
@@ -31,4 +36,27 @@ aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-c
 ansible-playbook ansible/jenkins-master.yml -i ansible/inventory/aws_ec2.yml --extra-vars 'hosts=tag_Name_${self.tags.Name}'
 EOF
   }
+}
+
+#create and bootstrap ec2 in us-east-1 tomcat
+resource "aws_instance" "jenkins-master-node" {
+  provider                      = aws.region-common
+  ami                           = data.aws_ssm_parameter.Ubuntu-us-east-1.value
+  instance_type                 = var.instance_type
+  key_name                      = aws_key_pair.common-key.key_name
+  associate_public_ip_address   = true
+  vpc_security_group_ids        = [aws_security_group.jenkins-sg.id]
+  subnet_id                     = aws_subnet.common_subnet_primary.id
+
+  tags = {
+    Name                        = "tomcat_server"
+  }
+}
+
+provisioner "local-exec" {
+  command                    = <<EOF
+aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-common} --instance-ids ${self.id}
+ansible-playbook ansible/jenkins-master.yml -i ansible/inventory/aws_ec2.yml --extra-vars 'hosts=tag_Name_${self.tags.Name}'
+EOF
+}
 }
