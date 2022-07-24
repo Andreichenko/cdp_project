dba6b241d36d7e5e1e8ff7183ea61ecb96959223
aleksandrandreichenko <andreichenko@web.de>
2022-07-24 22:41:52 +0300

    CDP-30-setup-a-docker-environment-using-terraform
    add
    + ec2 instance for docker server
    + sg for ec2 instance

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "jenkins-sg" {
 resource "aws_security_group" "tomcat-sg" {
   provider                 = aws.region-common
   name                     = "tomcat-sg"
-  description              = "Allow tcp/8081 tcp/22"
+  description              = "Allow tcp/8090 tcp/22"
   vpc_id                   = aws_vpc.vpc_common.id
   ingress {
     description            = "Allow 22 from our public IP"
@@ -44,6 +44,34 @@ resource "aws_security_group" "tomcat-sg" {
     from_port              = 8090
     protocol               = "tcp"
     to_port                = 8090
+    cidr_blocks            = [var.external_ip]
+  }
+
+  egress {
+    from_port              = 0
+    protocol               = "-1"
+    to_port                = 0
+    cidr_blocks            = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "docker-sg" {
+  provider                 = aws.region-common
+  name                     = "tomcat-sg"
+  description              = "Allow tcp/8080 tcp/22"
+  vpc_id                   = aws_vpc.vpc_common.id
+  ingress {
+    description            = "Allow 22 from our public IP"
+    from_port              = 22
+    protocol               = "tcp"
+    to_port                = 22
+    cidr_blocks            = [var.external_ip]
+  }
+  ingress {
+    description            = "Allow anyone on port 8081->8080/tcp"
+    from_port              = 8080
+    protocol               = "tcp"
+    to_port                = 8080
     cidr_blocks            = [var.external_ip]
   }
 
