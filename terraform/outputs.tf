1b62ea56fd809bb69f4feb574d64c1a0d1e203f5
aleksandrandreichenko <andreichenko@web.de>
2022-07-29 16:50:37 +0300

    CDP-51-create-an-infrastructure-for-kubernetes-in-aws-using-terraform-and-tf-modules
    + provider version requirements
    + variables for eks
    + cluster

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -26,4 +26,19 @@ output "cluster_id" {
   value = module.eks.cluster_id
 }
 
+output "cluster_endpoint" {
+  description = "Endpoint for your Kubernetes API server"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_status" {
+  description = "Status of the EKS cluster. One of `CREATING`, `ACTIVE`, `DELETING`, `FAILED`"
+  value       = module.eks.cluster_status
+}
+
+output "cluster_arn" {
+  description = "The Amazon Resource Name (ARN) of the cluster"
+  value       = module.eks.cluster_arn
+}
+
 
