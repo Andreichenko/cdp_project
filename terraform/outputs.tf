2a9761f733c6ec8cd9220fc3e3e84611516c3bb6
aleksandrandreichenko <andreichenko@web.de>
2022-07-29 15:44:39 +0300

    CDP-51-create-an-infrastructure-for-kubernetes-in-aws-using-terraform-and-tf-modules
    + provider version requirements
    + variables for eks
    + cluster

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -21,18 +21,18 @@ output "tomcat-server-node-public-ip" {
 output "docker-server-node-public-ip" {
   value = aws_instance.docker-server-node.public_ip
 }
-
-output "cluster_id" {
-  description = "The name/id of the EKS cluster. Will block on cluster creation until the cluster is really ready."
-  value       = local.cluster_id
-
-  # So that calling plans wait for the cluster to be available before attempting to use it.
-  # There is no need to duplicate this datasource
-  depends_on = [data.http.wait_for_cluster]
-}
-
-output "cluster_arn" {
-  description = "The Amazon Resource Name (ARN) of the cluster."
-  value       = local.cluster_arn
-}
+//
+//output "cluster_id" {
+//  description = "The name/id of the EKS cluster. Will block on cluster creation until the cluster is really ready."
+//  value       = local.cluster_id
+//
+//  # So that calling plans wait for the cluster to be available before attempting to use it.
+//  # There is no need to duplicate this datasource
+//  depends_on = [data.http.wait_for_cluster]
+//}
+//
+//output "cluster_arn" {
+//  description = "The Amazon Resource Name (ARN) of the cluster."
+//  value       = local.cluster_arn
+//}
 
