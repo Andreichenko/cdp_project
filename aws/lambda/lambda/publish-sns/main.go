e19e9a230fe381b753f82cbfed330e1ee7929bf0
aleksandrandreichenko <andreichenko@web.de>
2022-09-01 23:07:20 +0300

    CDP-59-lambda-function-for-sns-go
    added components
    sh for ec2

--- aws/lambda/lambda/publish-sns/main.go
+++ aws/lambda/lambda/publish-sns/main.go
@@ -6,14 +6,24 @@ import (
 	"fmt"
 	"log"
 	"strings"
+    "github.com/aws/aws-lambda-go/tree/main/lambda"
+    "github.com/aws/aws-lambda-go/tree/main/lambdacontext"
+	"github.com/aws/aws-sdk-go/tree/main/aws/session"
+	"github.com/aws/aws-sdk-go/tree/main/service/sns"
 
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-lambda-go/lambdacontext"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/sns"
 )
 
 func main() {
 	s := sns.New(session.New())
 
+	lambda.Start(func(ctx context.Context, js json.RawMessage) (string, error) {
+		c, ok := lambdacontext.FromContext(ctx)
+		if !ok {
+			log.Panicf("no context")
+		}
+
+		f := strings.Split(c.InvokedFunctionArn, ":")
+		if len(f) != 7 {
+			log.Panicf("cannot use arn: %s", c.InvokedFunctionArn)
+		}
 }
