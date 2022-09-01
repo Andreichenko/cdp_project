49957f3c85ea5f9f316954144927dae603748ae7
aleksandrandreichenko <andreichenko@web.de>
2022-09-01 22:34:31 +0300

    CDP-59-lambda-function-for-sns-go
    added components

--- aws/lambda/lambda/publish-sns/main.go
+++ aws/lambda/lambda/publish-sns/main.go
@@ -1,2 +1,15 @@
 package main
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sns"
+)
+
